c4d22aca06b9c53e6ab839fca95e34a7aa3ada20
AndrewTheBoss1 <andrewmondejarp@gmail.com>
2022-06-22 10:33:40 -0500

    Hi

--- src/pages/PageDetails.js
+++ src/pages/PageDetails.js
@@ -1,9 +1,23 @@
 import React from 'react'
 
-export default function PageDetails() {
+import { useParams } from 'react-router-dom'
+import useFetch from '../hooks/useFetch'
+
+
+
+export default function ReviewDetails() {
+
+  //should give us id
+  const { id } = useParams()
+  const { loading, error, data }  = useFetch('http://localhost:1337/api/units/' + id )
+
+  if (loading) return <p>Loading...</p>
+  if (loading) return <p>Error </p>
+ 
+
+
+  //displays on page 
   return (
-    <div>PageDetails</div>
+    
   )
 }
-
-//comment
