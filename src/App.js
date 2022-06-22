860d12cc191b4895688ca109f7510ffab4c3004a
AndrewTheBoss1 <andrewmondejarp@gmail.com>
2022-06-22 08:45:25 -0500

    App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
+// page & layout imports
+import Homepage from './pages/Homepage';
+import PageDetails from './pages/PageDetails';
+import SiteHeader from './components/SiteHeader';
 
 function App() {
   return (
-    <div className="App">
-     
-    </div>
+    <Router>
+      <div className="App">
+        <SiteHeader />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+
+          <Route path="/details/:id" element={<PageDetails />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
